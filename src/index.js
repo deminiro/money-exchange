d70cfb4c08969a4409c3edf394123b05e5c054a7
deminiro <43145879+deminiro@users.noreply.github.com>
2019-03-21 12:18:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,36 +2,66 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
-  let value = currency;
+  let result = {
+
+    }
+    let value = currency;
     if(currency<=10000 && currency>0){
-        for(var h=0,H=50;H<=currency;H++){
-            for(var q=0,Q=25;Q<=currency;Q++){
-                for(var d=0, D=10;D<=currency;D++){
-                    for(var n= 0, N=5;N<=currency;N++){
-                        for(var p=0, P=1;P<=currency;P++){
-                            value-=1;
-                            p++;
-                        }
-                        value-=5;
-                        n++;
-                    }
-                    value-=10;
-                    d++;
-                }
-                value-=25;
-                q++;
-            }
+        if(value>=50){
+        for(var h=0, H=50;currency-H>=0;H+=50){
             value-=50;
             h++;
         }
-        return {H:h,
-                Q:q,
-                D:d,
-                N:n,
-                P:P}
-    }else if(currency>10000){
-        return "error: "+'"'+ "You are rich, my friend! We don't have so much coins for exchange";
-    }else{
-        '{}';
+        if(h>0){
+             result.H = h;
+    }
+}
+     cur1 = value;
+    if(value>=25){
+        for(var q=0, Q=25;Q<=cur1;Q+=25){
+            cur1-=25;
+            q++;
+        }
+        if(q>0){
+            result.Q = q;
+    }
+}
+    cur2 = cur1;
+    if(cur1>=10){
+        for(var d=0, D=0;D<=cur2;D+=10){
+            cur2-=10;
+            d++;
+        }
+        if(d>0){
+            result.D = d;
+        }
+    }
+    cur3 = cur2;
+    if(cur3>=5){
+        for(var n=0, N=5;N<=cur3;N+=5){
+            cur3-=5;
+            n++;
+        }
+        if(n>0){
+            result.N = n;
+        }
+    }
+    cur4 = cur3
+    if(cur4>=0){
+        for(var p=0,P=0;P=cur4;P+=1){
+            cur4--;
+            p++;
+        }
+        if(p>0){
+            result.P = p;
+        }
+    }
+
+    return result;
+}   
+    else if(currency>10000){
+        return "{\nerror: "+'"You are rich, my friend! We don'+"'t have so much coins for exchange\n}";
+    }else if(currency<=0){
+        return result;
     }
 }
