1101b7b64a98b4c1d411e94542483f47f1bc774b
deminiro <43145879+deminiro@users.noreply.github.com>
2019-03-21 11:00:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,30 +2,54 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
-   let value = currency;
+  let value = currency;
     if(currency<=10000 && currency>0){
-        for(var h=0, H=50;H<=value;H+=50){
-            value - 50;
+        if(value>=50){
+        for(var h=0, H=50;currency-H>=0;H+=50){
+            value-=50;
             h++;
         }
-        for(var q=0, Q=25;Q<=value;Q+=25){
-            value - 25;
-            q=Math.floor(q/2)+1;
+        if(h>0){var cur1 = currency - value;}
+        else{ cur1 = value;}
+    }
+     
+    if(value>=25){
+        for(var q=0, Q=25;Q<=cur1;Q+=25){
+            cur1-=25;
+            q++;
         }
-        for(var d=0, D=10;D<=value;D+=10){
-            value - 10;
-            d=Math.floor(d/5)+1;
+        
+    }
+    
+    if(cur1>=10){
+        for(var d=0, D=10;D<=cur1;D+=10){
+            cur1-=10
+            d++;
         }
-        for(var n=0, N=5;N<=value;N+=5){
-            value - 5;
-            n=Math.floor(n/10)+1;
+    }
+    
+    if(cur1>=5){
+        for(var n=0, N=5;N<=cur1;N+=5){
+            cur1-=5;
+            n++;
         }
-        for(var p=0,P=1;P<=value;P+=1){
-            value - 1;
-            p=Math.floor(p/2)+1;
+    }
+    
+    if(cur1>=1){
+        for(var p=0,P=1;P<=cur1;P+=1){
+            cur1--;
+            p++;
         }
-        return 'H:'+ h + ' Q:' + q + ' D:' + d + ' N:' + n + ' P:' + p;
+    }
+    
+        return {H:h,
+                Q:q,
+                D:d,
+                N:n,
+                P:P}
+    }else if(currency>10000){
+        return "error: "+'"'+ "You are rich, my friend! We don't have so much coins for exchange";
     }else{
-        return "error:(You are rich, my friend! We don't have so much coins for exchange)";
+        '{}';
     }
 }
